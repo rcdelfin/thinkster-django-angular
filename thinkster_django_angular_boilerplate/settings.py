157303ffaea0cf6594bd0853a181230b28b9f303
James Brewer <james@jamesbrewer.io>
2014-11-17 10:09:39 -0800

    Update settings.AUTH_USER_MODEL and use Django REST Framework Router in urls.py.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -117,3 +117,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+AUTH_USER_MODEL = 'authentication.Account'
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,28 +1,21 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, patterns, url
 from django.views.generic.base import TemplateView
 
-from authentication.views import LoginView, LogoutView, UserCreateView, \
-    UserDestroyView, UserProfileRetrieveUpdateView
-from posts.views import PostListCreateView, \
-    PostRetrieveUpdateDestroyView, UserPostsListView
+from rest_framework.routers import DefaultRouter
+
+from authentication.views import AccountViewSet, LoginView, LogoutView
+from posts.views import PostViewSet
+
+router = DefaultRouter()
+router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = patterns(
     '',
 
-    url(r'^api/v1/users/$', UserCreateView.as_view(), name='user-create'),
-    url(r'^api/v1/users/(?P<pk>[0-9]+)/$',
-        UserDestroyView.as_view(), name='user-destroy'),
-    url(r'^api/v1/users/(?P<user__username>[a-zA-Z0-9_@+-]+)/$',
-        UserProfileRetrieveUpdateView.as_view(), name='profile'),
-    url(r'^api/v1/users/(?P<username>[a-zA-Z0-9_@+-]+)/posts/$',
-        UserPostsListView.as_view(), name='profile-posts'),
-
+    url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
-
-    url(r'^api/v1/posts/$', PostListCreateView.as_view(), name='posts'),
-    url(r'^api/v1/posts/(?P<pk>[0-9]+)/$',
-        PostRetrieveUpdateDestroyView.as_view(), name='post'),
 
     url(r'^.*$',
         TemplateView.as_view(template_name='index.html'), name='index'),
