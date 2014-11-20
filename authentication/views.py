92923d4c2ce89743d78c72e4a4621bfa0d133052
James Brewer <james@jamesbrewer.io>
2014-11-20 09:40:45 -0800

    Make sure Account passwords are stored correctly.

--- authentication/views.py
+++ authentication/views.py
@@ -18,7 +18,27 @@ class AccountViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.request.method in permissions.SAFE_METHODS:
             return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
         return (permissions.IsAuthenticated(), IsAccountOwner(),)
+
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.DATA)
+
+        if serializer.is_valid():
+            account = Account.objects.create_user(**request.DATA)
+
+            account.set_password(request.DATA.get('password'))
+            account.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(views.APIView):
